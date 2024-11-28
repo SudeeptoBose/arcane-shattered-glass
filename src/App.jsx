b235f1627c6dcd379a957937a7d5ddfe21c68518
SudeeptoBose <sudeeptobose23@gmail.com>
2024-11-28 21:44:01 +0600

    scroll edits

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import { Canvas } from "@react-three/fiber"
 import Experience from "./Experience"
 import Footer from "./Footer"
 import AudioPlayer from "./AudioPlayer"
+import { useEffect } from "react"
 
 function App() {
 
+	useEffect(() => {
+		document.body.style.overscrollBehavior = 'none';
+		return () => {
+		  document.body.style.overscrollBehavior = 'auto';
+		};
+	  }, []);
 	return (
 		<>
 			<Canvas>
